435d4493982e220720fa0ad434ef94ec63a40e9a
thien2002nguyen <133083833+thien2002nguyen@users.noreply.github.com>
2024-04-25 13:09:23 +0700

    [update] insert data test with service

--- server/controllers/insert.js
+++ server/controllers/insert.js
@@ -1,67 +1,91 @@
-const User = require('../models/user')
-const Admin = require('../models/admin')
-const Room = require('../models/room')
-const asyncHandler = require('express-async-handler')
-const { userData, adminData, roomData } = require('../data/dataTest')
+const User = require("../models/user");
+const Admin = require("../models/admin");
+const Room = require("../models/room");
+const Service = require("../models/service");
+const asyncHandler = require("express-async-handler");
+const {
+  userData,
+  adminData,
+  roomData,
+  serviceData,
+} = require("../data/dataTest");
 
 const fnInsertUser = async (user) => {
-    await User.create({
-        userId: user.userId,
-        name: user.name,
-        birthday: user.birthday,
-        address: user.address,
-        classStudy: user.classStudy,
-        email: user.email,
-        phone: user.phone,
-        password: '123456'
-    })
-}
+  await User.create({
+    userId: user.userId,
+    name: user.name,
+    birthday: user.birthday,
+    address: user.address,
+    classStudy: user.classStudy,
+    email: user.email,
+    phone: user.phone,
+    password: "123456",
+  });
+};
 
 const insertUsers = asyncHandler(async (req, res) => {
-    const promises = []
-    for (let user of userData) {
-        promises.push(fnInsertUser(user))
-    }
-    await Promise.all(promises)
-    return res.status(200).json('Done')
-})
+  const promises = [];
+  for (let user of userData) {
+    promises.push(fnInsertUser(user));
+  }
+  await Promise.all(promises);
+  return res.status(200).json("Done");
+});
 
 const fnInsertAdmin = async (admin) => {
-    await Admin.create({
-        email: admin.email,
-        name: admin.name,
-        password: '123456'
-    })
-}
+  await Admin.create({
+    email: admin.email,
+    name: admin.name,
+    password: "123456",
+  });
+};
 
 const insertAdmins = asyncHandler(async (req, res) => {
-    const promises = []
-    for (let admin of adminData) {
-        promises.push(fnInsertAdmin(admin))
-    }
-    await Promise.all(promises)
-    return res.status(200).json('Done')
-})
+  const promises = [];
+  for (let admin of adminData) {
+    promises.push(fnInsertAdmin(admin));
+  }
+  await Promise.all(promises);
+  return res.status(200).json("Done");
+});
 
 const fnInsertRoom = async (room) => {
-    await Room.create({
-        numberRoom: room.numberRoom,
-        max_people: room.max_people,
-        roomprice: room.roomprice,
-    })
-}
+  await Room.create({
+    numberRoom: room.numberRoom,
+    max_people: room.max_people,
+    roomprice: room.roomprice,
+  });
+};
 
 const insertRooms = asyncHandler(async (req, res) => {
-    const promises = []
-    for (let room of roomData) {
-        promises.push(fnInsertRoom(room))
-    }
-    await Promise.all(promises)
-    return res.status(200).json('Done')
-})
+  const promises = [];
+  for (let room of roomData) {
+    promises.push(fnInsertRoom(room));
+  }
+  await Promise.all(promises);
+  return res.status(200).json("Done");
+});
+
+const fnInsertService = async (service) => {
+  await Service.create({
+    name: service.name,
+    price: service.price,
+    description: service.description,
+  });
+};
+
+const insertServices = asyncHandler(async (req, res) => {
+  const promises = [];
+  for (let service of serviceData) {
+    promises.push(fnInsertService(service));
+  }
+  await Promise.all(promises);
+  return res.status(200).json("Done");
+});
 
 module.exports = {
-    insertUsers,
-    insertAdmins,
-    insertRooms
-}
+  insertUsers,
+  insertAdmins,
+  insertRooms,
+  insertServices,
+};
--- server/routers/insert.js
+++ server/routers/insert.js
@@ -1,8 +1,9 @@
-const router = require('express').Router()
-const ctrls = require('../controllers/insert')
+const router = require("express").Router();
+const ctrls = require("../controllers/insert");
 
-router.post('/user', ctrls.insertUsers)
-router.post('/admin', ctrls.insertAdmins)
-router.post('/room', ctrls.insertRooms)
+router.post("/user", ctrls.insertUsers);
+router.post("/admin", ctrls.insertAdmins);
+router.post("/room", ctrls.insertRooms);
+router.post("/service", ctrls.insertServices);
 
-module.exports = router
+module.exports = router;
